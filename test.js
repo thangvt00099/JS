16da60a754b0b40683eef3c159ec49edb713ae4d
thangvt00099 <tranvuthang57@gmail.com>
2024-09-10 20:33:54 +0700

    Filter method

--- test.js
+++ test.js
@@ -1,39 +1,12 @@
 /**
-Syntax: reduce(function, [initialValue])
-1. function: Một hàm để thực thi cho từng phần tử trong mảng
-2. initialValue: Giá trị khởi tạo
+Syntax: array.filter(function)
+	function(currentValue, [currentIndex], [Array])
+	1. currentValue: Giá trị hiện tại đang xử lý trong mảng
+	2. currentIndex: (Tùy chọn) Chỉ số của phần tử đang xét
+	3. Array: (Tùy chọn) Mảng gọi phương thức filter
  */
-
-// Bài toán khi không dùng reduce()
-let M1 = [1, 2, 3];
-let sum = 0;
-for (let element of M1) {
-	sum += element;
-}
-console.log(sum);
-
-/**
-accumulator: Giá trị tích lũy, được cập nhật sau mỗi lần gọi hàm
-currentValue: Giá trị hiện tại đang được xử lý trong mảng
-currentIndex: (Tùy chọn) Chỉ số của phần tử đang xử lý
-array: (Tùy chọn) Mảng đang được reduce - chính là cái mảng đang gọi reduce()
- */
-
-/**
-1. Tham số thứ 2: initialValue giá trị khởi tạo của accumulator ban đầu
-2. Nếu bỏ trống initialValue:
-	a. accumulator: tích lũy sẽ lấy giá trị đầu tiên trong mảng làm giá trị khởi tạo,
-	và sẽ bắt đầu thực hiện từ phần tử thứ 2.
-	b. Nếu mảng bỏ trống mà 0 có giá trị initialValue => reduce() báo lỗi
- */
-// Khi dùng reduce()
-let sum2 = M1.reduce(
-	(accumulator, currentValue, currentIndex, array) => {
-		return accumulator + currentValue;
-	}
-);
-console.log(sum2)
-
-// Rút gọn
-let sum3 = M1.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(sum3);
+let number = [1, 2, 3, 4, 5];
+let evenNums = number.filter(function(currentValue) {
+	return currentValue % 2 === 0;
+});
+console.log(evenNums)
