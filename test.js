dcca60f401378d03a31f101c9cb88f75c542c5bf
thangvt00099 <tranvuthang57@gmail.com>
2024-09-10 20:21:16 +0700

    Reduce method

--- test.js
+++ test.js
@@ -1,31 +1,39 @@
-// Phương thức sort()
-// Khi không truyền compareFunction
-let M1 = ["b", "a", "c"];
-// Xem giá trị unicode dùng charCodeAt()
-console.log("b".charCodeAt());
-// Duyệt mảng
+/**
+Syntax: reduce(function, [initialValue])
+1. function: Một hàm để thực thi cho từng phần tử trong mảng
+2. initialValue: Giá trị khởi tạo
+ */
+
+// Bài toán khi không dùng reduce()
+let M1 = [1, 2, 3];
+let sum = 0;
 for (let element of M1) {
-	console.log(`${element} có mã unicode: ${element.charCodeAt()}`);
+	sum += element;
 }
+console.log(sum);
 
-let sortedM1 = M1.sort();
-console.log(sortedM1);
+/**
+accumulator: Giá trị tích lũy, được cập nhật sau mỗi lần gọi hàm
+currentValue: Giá trị hiện tại đang được xử lý trong mảng
+currentIndex: (Tùy chọn) Chỉ số của phần tử đang xử lý
+array: (Tùy chọn) Mảng đang được reduce - chính là cái mảng đang gọi reduce()
+ */
 
-// TH2: So sánh phần tử có nhiều kí tự
-let M2 = ["baa", "a", "c"];
-let sortedM2 = M2.sort();
-console.log(sortedM2);
+/**
+1. Tham số thứ 2: initialValue giá trị khởi tạo của accumulator ban đầu
+2. Nếu bỏ trống initialValue:
+	a. accumulator: tích lũy sẽ lấy giá trị đầu tiên trong mảng làm giá trị khởi tạo,
+	và sẽ bắt đầu thực hiện từ phần tử thứ 2.
+	b. Nếu mảng bỏ trống mà 0 có giá trị initialValue => reduce() báo lỗi
+ */
+// Khi dùng reduce()
+let sum2 = M1.reduce(
+	(accumulator, currentValue, currentIndex, array) => {
+		return accumulator + currentValue;
+	}
+);
+console.log(sum2)
 
-// Ví dụ với kí tự số => kết quả ko như mong muốn
-let M3 = [10000, 1, 9];
-let sortedM3 = M3.sort();
-console.log(sortedM3);
-
-// Khi truyền compareFunction
-// TH1: compareFunction trả về giá trị < 0 => a sẽ đặt trước b
-// TH2: compareFunction trả về 0 => thứ tự giữa a và b không thay đổi
-// TH3: compareFunction trả về giá trị > 0 => b sẽ đặt trước a
-let M4 = [9, 3, 5, 4];
-console.log("Trước khi sắp xếp: ", M4);
-let sortedM4 = M4.sort((a, b) => a - b)
-console.log("Sau khi sắp xếp: ", sortedM4);
+// Rút gọn
+let sum3 = M1.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log(sum3);
