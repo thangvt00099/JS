4849818198376f657ef146be196348040665f8e5
thangvt00099 <tranvuthang57@gmail.com>
2024-09-12 16:40:54 +0700

    OOP - Section 1

--- test.js
+++ test.js
@@ -1,12 +1,67 @@
-/**
-Syntax: array.filter(function)
-	function(currentValue, [currentIndex], [Array])
-	1. currentValue: Giá trị hiện tại đang xử lý trong mảng
-	2. currentIndex: (Tùy chọn) Chỉ số của phần tử đang xét
-	3. Array: (Tùy chọn) Mảng gọi phương thức filter
- */
-let number = [1, 2, 3, 4, 5];
-let evenNums = number.filter(function(currentValue) {
-	return currentValue % 2 === 0;
-});
-console.log(evenNums)
+// OOP - Phần 1
+
+// 1. Khai báo đối tượng, literal syntax
+let student = {
+	// Thuộc tính
+	fullName: "Trần Vũ Thắng",
+	birthYear: 2003,
+	address: {
+		city: "Hà Nội",
+		country: "Việt Nam"
+	},
+	scores: [8, 9, 10],
+
+	// Phương thức
+	getAge: function() {
+		return 2024 - this.birthYear;
+	},
+
+	diemTrungBinh: function() {
+		let sumScores = this.scores.reduce((a, b) => a + b, 0);
+		return sumScores / 3;
+	}
+}
+
+// Kiểm tra kiểu loại
+console.log(typeof student);
+
+// OOP - Phần 2
+// 2. Truy cập thuộc tính: Dot Notation . và Braket Notation []
+// 2.1 Sử dụng Dot Notation (.)
+console.log(student.fullName);
+console.log(student.address);
+console.log(student.scores);
+
+// 2.2 Sử dụng Braket Notation []
+console.log(student["fullName"]);
+console.log(student["address"]);
+
+// let inputKey = prompt("Mời nhập key: (address, scores)");
+let inputKey = "address";
+if (student[inputKey]) {
+	console.log(student[inputKey]);
+} else {
+	console.log("Key bạn nhập không tồn tại!");
+}
+
+// OOP - Phần 3
+// 3.1 Truy cập phương thức
+console.log(student.getAge());
+console.log(student.diemTrungBinh());
+
+// OOP - Phần 4
+// 4. Thêm, xóa thuộc tính
+student.email = "tranvuthang57@gmail.com";
+console.log(student);
+student["website"] = "http://github.com";
+console.log(student);
+console.log(student.email);
+console.log(student.website);
+
+// Xóa thuộc tính
+delete student.email;
+console.log(student);
+
+// Sửa thuộc tính
+student.website = "Changed Value";
+console.log(student);
