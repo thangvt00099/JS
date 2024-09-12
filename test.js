ab722b4b4ae3477f8d7c66d687b9c2ed81bafd32
thangvt00099 <tranvuthang57@gmail.com>
2024-09-12 16:53:53 +0700

    Object JS Method 1

--- test.js
+++ test.js
@@ -1,67 +1,42 @@
-// OOP - Phần 1
+// Object JS Method - Phần 1
+// Một số phương thức có sẵn trong JS
+// 1. Object.keys(obj) => phương thức trả về 1 mảng chứa các key của đối tượng
+let person = {
+	name: "Trần Vũ Thắng",
+	age: 21
+};
+let keys = Object.keys(person);
+console.log(keys);
 
-// 1. Khai báo đối tượng, literal syntax
-let student = {
-	// Thuộc tính
-	fullName: "Trần Vũ Thắng",
-	birthYear: 2003,
-	address: {
-		city: "Hà Nội",
-		country: "Việt Nam"
-	},
-	scores: [8, 9, 10],
+// 2. Object.values(obj) => phương thức trả về 1 mảng chứa các giá trị của đối tượng
+let person2 = {
+	name: "Trần Vũ Thắng",
+	age: 21
+};
+let values = Object.values(person2);
+console.log(values);
 
-	// Phương thức
-	getAge: function() {
-		return 2024 - this.birthYear;
-	},
+// 3. Object.entries(obj) => phương thức trả về 1 mảng chứa các cặp key-value của đối tượng dưới dạng mảng con
+let person3 = {
+	name: "Trần Vũ Thắng",
+	age: 21
+};
+let entries = Object.entries(person3);
+console.log(entries);
 
-	diemTrungBinh: function() {
-		let sumScores = this.scores.reduce((a, b) => a + b, 0);
-		return sumScores / 3;
-	}
-}
+// 4. Object.assign(target, source) => sao chép các thuộc tính từ 1 hoặc nhiều đối tượng nguồn(source) vào một đối tượng đích(target)
+let target = {
+	c: 100
+};
+let source1 = {
+	a: 1
+};
+let source2 = {
+	b: 2
+};
+Object.assign(target, source1, source2);
+console.log(target);
 
-// Kiểm tra kiểu loại
-console.log(typeof student);
-
-// OOP - Phần 2
-// 2. Truy cập thuộc tính: Dot Notation . và Braket Notation []
-// 2.1 Sử dụng Dot Notation (.)
-console.log(student.fullName);
-console.log(student.address);
-console.log(student.scores);
-
-// 2.2 Sử dụng Braket Notation []
-console.log(student["fullName"]);
-console.log(student["address"]);
-
-// let inputKey = prompt("Mời nhập key: (address, scores)");
-let inputKey = "address";
-if (student[inputKey]) {
-	console.log(student[inputKey]);
-} else {
-	console.log("Key bạn nhập không tồn tại!");
-}
-
-// OOP - Phần 3
-// 3.1 Truy cập phương thức
-console.log(student.getAge());
-console.log(student.diemTrungBinh());
-
-// OOP - Phần 4
-// 4. Thêm, xóa thuộc tính
-student.email = "tranvuthang57@gmail.com";
-console.log(student);
-student["website"] = "http://github.com";
-console.log(student);
-console.log(student.email);
-console.log(student.website);
-
-// Xóa thuộc tính
-delete student.email;
-console.log(student);
-
-// Sửa thuộc tính
-student.website = "Changed Value";
-console.log(student);
+// Sử dụng toán tử spread để sao chép thuộc tính
+let target2 = {...source1, ...source2};
+console.log(target2);
