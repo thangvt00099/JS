0b19a2ac64f990ffed4ff43de10000483d6170b1
thangvt00099 <tranvuthang57@gmail.com>
2024-09-13 15:27:08 +0700

    Const or Let in declaration object

--- test.js
+++ test.js
@@ -1,74 +1,22 @@
-// Object JS Method - Phần 1
-// Một số phương thức có sẵn trong JS
-// 1. Object.keys(obj) => phương thức trả về 1 mảng chứa các key của đối tượng
+// Sử dụng let hay const để khai báo đối tượng
+// Khai báo đối tượng sử dụng let
 let person = {
 	name: "Trần Vũ Thắng",
-	age: 21
+	age: 30
 };
-let keys = Object.keys(person);
-console.log(keys);
+person = 5;
+console.log(person);
 
-// 2. Object.values(obj) => phương thức trả về 1 mảng chứa các giá trị của đối tượng
-let person2 = {
+// Khai báo đối tượng sử dụng const
+const person2 = {
 	name: "Trần Vũ Thắng",
-	age: 21
+	age: 30
 };
-let values = Object.values(person2);
-console.log(values);
+// person2 = 5;
+console.log(person2);
 
-// 3. Object.entries(obj) => phương thức trả về 1 mảng chứa các cặp key-value của đối tượng dưới dạng mảng con
-let person3 = {
-	name: "Trần Vũ Thắng",
-	age: 21
-};
-let entries = Object.entries(person3);
-console.log(entries);
-
-// 4. Object.assign(target, source) => sao chép các thuộc tính từ 1 hoặc nhiều đối tượng nguồn(source) vào một đối tượng đích(target)
-let target = {
-	c: 100
-};
-let source1 = {
-	a: 1
-};
-let source2 = {
-	b: 2
-};
-Object.assign(target, source1, source2);
-console.log(target);
-
-// Sử dụng toán tử spread để sao chép thuộc tính
-let target2 = {...source1, ...source2};
-console.log(target2);
-
-// Object JS Method - Phần 2
-// 5. Object.hasOwnProperty(pro) => phương thức này kiểm tra xem đối tượng có thuộc tính đó hay không (True or False)
-let person4 = {
-	name: "Trần Vũ Thắng",
-	age: 21
-};
-console.log(person4.hasOwnProperty("name"));
-console.log(person4.hasOwnProperty("job"));
-
-// 6. Object.freeze(obj) => phương thức này đóng băng đối tượng, không thể thêm - sửa - xóa
-let person5 = {
-	name: "Trần Vũ Thắng",
-	age: 21
-};
-Object.freeze(person5);
-person5.age = 100;
-person5.email = "tranvuthang57@gmail.com";
-delete person5.age;
-console.log(person5);
-
-// 7. Object.seal(obj) => phương thức này không cho phép thêm - xóa, chỉ cho phép sửa
-let person6 = {
-	name: "Trần Vũ Thắng",
-	age: 21
-};
-Object.seal(person6);
-person6.location = "New York";
-delete person6.age;
-person6.age = 100;
-console.log(person6);
+// Vẫn thêm, sửa, xóa thuộc tính bình thường
+// Chỉ không ghi đè lại được đối tượng
+person2.isAdmin = true;
+console.log(person2);
 
