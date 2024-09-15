16e7cb5b088352648f64e555b48d257a727be9b6
thangvt00099 <tranvuthang57@gmail.com>
2024-09-15 20:36:13 +0700

    ES6 Constructor OOP - Class

--- test.js
+++ test.js
@@ -1,36 +1,34 @@
-// Prototype trong JS
-function Student (fullName, ID, birthYear, homeTown) {
-	this.fullName = fullName;
-	this.ID = ID;
-	this.birthYear = birthYear;
-	this.homeTown = homeTown;
+// ES 6 Classes
+// Bản chất JS không có class, Class chỉ là 1 hàm đặc biệt và tuân theo kế thừa từ Prototype
+// Class không hoisted (Không tự đưa khai báo lên đầu)
+// 1. Class Expression (ít dùng)
+const SinhVienEx = class {
 
-	this.showInfo = function() {
-		return `${this.fullName} - ${this.ID} - ${this.birthYear} - ${this.homeTown}`;
+};
+
+// 2. Class Declaration (dùng chủ yếu)
+class SinhVien {
+	constructor(firstName, ID, birthYear, homeTown) {
+		this.firstName = firstName;
+		this.ID = ID;
+		this.birthYear = birthYear;
+		this.homeTown = homeTown;
 	}
-};
 
-const sv1 = new Student("Trần Vũ Thắng", "2021600099", 2003, "Hà Nội");
-const sv2 = new Student("Phạm Quốc Thái", "2021612399", 2003, "Hà Nội");
+	showInfo() {
+		return `${this.firstName}/${this.ID}/${this.homeTown}`;
+	}
+}
 
-// Kiểm tra nguyên mẫu cha mẹ của đối tượng
-// 4. Xem nguyên mẫu
-console.log(Object.getPrototypeOf(sv1));
-console.log(Student.prototype);
-console.log(Student.prototype === Object.getPrototypeOf(sv1));
-console.log(Student.prototype.isPrototypeOf(sv2));
+// Tạo đối tượng
+const stu3 = new SinhVien("Trần Vũ Thắng", 1, 2003, "Hà Nội");
+const stu4 = new SinhVien("Phạm Quốc Thái", 2, 2003, "Hà Nội");
+console.log(stu3);
+console.log(Object.getPrototypeOf(stu3));
+console.log(Object.getPrototypeOf(stu3) === SinhVien.prototype);
 
-// 5. Thêm phương thức với prototype
-Student.prototype.calAge = function() {
-	console.log(2024 - this.birthYear);
-};
-console.log(sv1);
-
-// 6. Thêm thuộc tính
-Student.prototype.email = "noemail@gmail.com";
-console.log(sv1.email);
-console.log(sv2.email);
-
-// Kiểm tra 1 đối tượng có trực tiếp chứa thuộc tính không
-console.log(sv1.hasOwnProperty("email"));
-console.log(sv1.hasOwnProperty("fullName"));
+// Thêm phương thức vào class SinhVien
+SinhVien.prototype.calAge = function(currentYear) {
+	return currentYear - this.birthYear;
+}
+console.log(stu3.calAge(2024));
