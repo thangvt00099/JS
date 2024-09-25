7d25a2f95464e97be8360e27b9629e4c879c4071
thangvt00099 <tranvuthang57@gmail.com>
2024-09-25 16:27:43 +0700

    textContent - innerHTML - innerText

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-function updateClock() {
-	const now = new Date();
-	const hours = String(now.getHours()).padStart(2, "0");
-	const minutes = String(now.getMinutes()).padStart(2, "0");
-	const seconds = String(now.getSeconds()).padStart(2, "0");
-	const timeString = `${hours}:${minutes}:${seconds}`;
-	let clockElement = document.querySelector(".clock-container .clock")
-	clockElement.innerHTML = timeString;
-}
-setInterval(updateClock, 1000);
+// DOM - textContent, innerHTML, innerText
+
+let element = document.getElementById("demo");
+
+// textContent - Trả về nội dung nằm bên trong phần tử, bao gồm cả khoảng trắng, tab xuống dòng,...
+console.log("textContent: ");
+console.log(element.textContent);
+
+// innerText - Trả về văn bản mà người dùng có thể nhìn thấy trên trình duyệt, loại bỏ các phần tử được ẩn bằng CSS
+console.log("innerText: ");
+console.log(element.innerText);
+
+// innerHTML - Trả về toàn bộ nội dung HTML của phần tử, bao gồm cả các thẻ HTML và cấu trúc của chúng
+console.log("innerHTML: ");
+console.log(element.innerHTML);
+
+let element2 = document.getElementById("demo2");
+element2.innerHTML = "<p><i>Tuhoc.cc</i></p>";
+element2.textContent = "<p><i>Tuhoc.cc</i></p>";
+
+
