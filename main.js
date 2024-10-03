d576b9b48b1abadd51ad50fd8ffa13a2f5ca3d75
thangvt00099 <tranvuthang57@gmail.com>
2024-10-03 15:57:30 +0700

    This Keyword

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-const btn = document.querySelector("#btn");
-const h1Elm = document.querySelector("#title");
-function changeColor() {
-	const redColor = Math.floor(Math.random() * 256);
-	const greenColor = Math.floor(Math.random() * 256);
-	const blueColor = Math.floor(Math.random() * 256);
-	return `rgb(${redColor},${greenColor},${blueColor})`;
+// This keyword
+function randomColor() {
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+	return `rgb(${r},${g},${b})`;
 }
 
-btn.addEventListener("click", function() {
-	document.body.style.backgroundColor = changeColor();
-	h1Elm.textContent = changeColor();
-})
+const btnList = document.querySelectorAll(".btn");
+// for (let btn of btnList) {
+// 	if (btn instanceof HTMLElement) {
+// 		btn.addEventListener("click", function() {
+// 			btn.style.background = randomColor();
+// 			btn.style.color = randomColor();
+// 		})
+// 	}
+// }
+
+const titles = document.querySelectorAll(".title");
+// for (let title of titles) {
+// 	if (title instanceof HTMLElement) {
+// 		title.addEventListener("click", function() {
+// 			title.style.background = randomColor();
+// 			title.style.color = randomColor();
+// 		})
+// 	}
+// }
+
+function changColor() {
+	this.style.background = randomColor();
+	this.style.color = randomColor();
+}	
+
+for (let btn of btnList) {
+	btn.addEventListener("click", changColor)
+}
+for (let title of titles) {
+	title.addEventListener("click", changColor)
+}
