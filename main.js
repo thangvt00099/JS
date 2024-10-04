b56d1dbfbdee64ba9214318815323ff5be309113
thangvt00099 <tranvuthang57@gmail.com>
2024-10-04 17:53:29 +0700

    Keyboard Events And Event Object

--- main.js
+++ main.js
@@ -1,39 +1,21 @@
-// This keyword
-function randomColor() {
-	const r = Math.floor(Math.random() * 256);
-	const g = Math.floor(Math.random() * 256);
-	const b = Math.floor(Math.random() * 256);
-	return `rgb(${r},${g},${b})`;
-}
+// Keyboard events and event object
+const inp = document.querySelector("#inp");
+// inp.addEventListener("keydown", function() {
+// 	console.log("KEYDOWN!")
+// })
 
-const btnList = document.querySelectorAll(".btn");
-// for (let btn of btnList) {
-// 	if (btn instanceof HTMLElement) {
-// 		btn.addEventListener("click", function() {
-// 			btn.style.background = randomColor();
-// 			btn.style.color = randomColor();
-// 		})
-// 	}
-// }
+// inp.addEventListener("keyup", function() {
+// 	console.log("KEYUP!")
+// })
 
-const titles = document.querySelectorAll(".title");
-// for (let title of titles) {
-// 	if (title instanceof HTMLElement) {
-// 		title.addEventListener("click", function() {
-// 			title.style.background = randomColor();
-// 			title.style.color = randomColor();
-// 		})
-// 	}
-// }
+// Event object
+// inp.addEventListener("keydown", function(e) {
+// 	console.log(e);
+// })
 
-function changColor() {
-	this.style.background = randomColor();
-	this.style.color = randomColor();
-}	
+document.addEventListener("keydown", function(e) {
+	if (e.code === "KeyR") {
+		document.body.style.backgroundColor = "cyan";	
+	}
+})
 
-for (let btn of btnList) {
-	btn.addEventListener("click", changColor)
-}
-for (let title of titles) {
-	title.addEventListener("click", changColor)
-}
