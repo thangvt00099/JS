30e2b198545cbf3166946500e926e3eaaa92e757
thangvt00099 <tranvuthang57@gmail.com>
2024-10-08 18:11:31 +0700

    Event Delegation

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-// Event Bubbling - stopPropagation
-// stopPropagation - ngăn chặn sự kiện ra các thẻ cha, không ngăn ngừa sự kiện của 1 element
-// stopImmediatePropagation - ngăn chặn sự kiện của cùng 1 element
-// capturing - ngược lại bubbling
-const child = document.querySelector("#child");
-const parent = document.querySelector("#parent");
-const grandparent = document.querySelector("#grandparent");
+// Event Delegation: Sự kiện được giao phó từ phần tử cha => phần tử con
+/*
+Cách thực hiện:
+	1. Thêm sự kiện vào phần tử cha
+	2. Kiểm tra xem phần tử nào xảy ra sự kiện (event.target)
+	3. Thực hiện công việc cần thiết
+*/
 
-// child.addEventListener("click", function(e) {
-// 	console.log("Child clicked");
-// }, {capture: true})
+let formElm = document.querySelector("#form");
+let ulElm = document.querySelector("#list_item");
+formElm.addEventListener("submit", function(e) {
+	e.preventDefault();
+	let username = document.querySelector("#username").value;
+	let message = document.querySelector("#message").value;
+	let liElm = document.createElement("li");
+	liElm.innerText = `${username} - ${message}`;
+	ulElm.appendChild(liElm);
 
-// parent.addEventListener("click", function(e) {
-// 	console.log("Parent clicked");
-// }, {capture: true})
+	// reset form
+	formElm.reset();
+}) 
 
-// grandparent.addEventListener("click", function() {
-// 	console.log("Grand parent clicked");
-// }, {capture: true})
+// Xóa thẻ li khi click vào, không dùng event delegation
+// const liList = document.querySelectorAll("li");
+// console.log(liList);
 
-child.addEventListener("click", function(e) {
-	e.stopPropagation();
-	console.log("Clicked")
+// for (let i = 0; i < liList.length; i++) {
+// 	liList[i].addEventListener("click", function() {
+// 		liList[i].remove();
+// 	})
+// }
+
+// Vấn đề: khi thêm 1 li mới vào ul, thì li đó không có sự kiện click
+// Xóa phần tử li khi click vào dùng event delegation
+ulElm.addEventListener("click", function(e) {
+	const li = e.target;
+	// Kiểm tra xem thẻ được có phải li không
+	if (li.tagName === "LI") {
+		li.remove();
+	}
 })
-
-child.addEventListener("click", function(e) {
-	e.stopPropagation();
-	console.log("Clicked-2")
-})
-
-
