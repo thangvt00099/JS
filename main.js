ddbede4b2ba4fe40498a342335ba45589d33bd00
thangvt00099 <tranvuthang57@gmail.com>
2024-10-08 17:46:52 +0700

    Capturing - stopImmediatePropagation

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-// Event Bubbling
+// Event Bubbling - stopPropagation
+// stopPropagation - ngăn chặn sự kiện ra các thẻ cha, không ngăn ngừa sự kiện của 1 element
+// stopImmediatePropagation - ngăn chặn sự kiện của cùng 1 element
+// capturing - ngược lại bubbling
 const child = document.querySelector("#child");
 const parent = document.querySelector("#parent");
 const grandparent = document.querySelector("#grandparent");
 
+// child.addEventListener("click", function(e) {
+// 	console.log("Child clicked");
+// }, {capture: true})
+
+// parent.addEventListener("click", function(e) {
+// 	console.log("Parent clicked");
+// }, {capture: true})
+
+// grandparent.addEventListener("click", function() {
+// 	console.log("Grand parent clicked");
+// }, {capture: true})
+
 child.addEventListener("click", function(e) {
 	e.stopPropagation();
-	console.log("Child clicked");
+	console.log("Clicked")
 })
 
-parent.addEventListener("click", function(e) {
+child.addEventListener("click", function(e) {
 	e.stopPropagation();
-	console.log("Parent clicked");
-})
-
-grandparent.addEventListener("click", function() {
-	console.log("Grand parent clicked");
+	console.log("Clicked-2")
 })
 
 
